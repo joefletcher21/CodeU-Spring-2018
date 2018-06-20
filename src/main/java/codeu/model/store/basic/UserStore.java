97e7489ee3abbbb4a3355a0548b58d9232b316fc
Blair Bucshon <blairbucshon@Blairs-MacBook-Pro.local>
2018-06-19 21:19:45 -0400

    adding in stats on admin page

--- src/main/java/codeu/model/store/basic/UserStore.java
+++ src/main/java/codeu/model/store/basic/UserStore.java
@@ -93,6 +93,11 @@ public class UserStore {
     return null;
   }
 
+  /** Access the current set of users known to the application. */
+  public List<User> getAllUsers() {
+    return users;
+  }
+
   /**
    * Add a new user to the current set of users known to the application. This should only be called
    * to add a new user, not to update an existing user.
